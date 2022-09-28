ad23527eda6860d8186d704df6f0467ab916446d
Xiaoqi Zhang <90387077+XiaoqZhang@users.noreply.github.com>
2022-09-28 08:46:21 +0200

    use array to calculate distances and potentials

--- nvt_results/ABUWOJ_clean/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh -f
-export RASPA_DIR=/home/xiaoqi/RASPA/simulations/
-$RASPA_DIR/bin/simulate simulation.input
--- run_weight.py
+++ run_weight.py
@@ -2,7 +2,7 @@ from cmath import nan
 from copy import copy
 import os
 import re
-from turtle import update
+from turtle import distance, update
 import numpy as np 
 import pandas as pd
 import math
@@ -11,6 +11,8 @@ import json
 import concurrent.futures
 
 from pymatgen.core import Structure
+
+from sklearn.preprocessing import MinMaxScaler
 
 import matplotlib.pyplot as plt
 
@@ -31,11 +33,11 @@ threshold = 20
 
 #nvt_path = "/run/user/1001/gvfs/smb-share:server=lsmosrv2.epfl.ch,share=xiazhang/core_ch4_nvt"
 #nvt_path = "./nvt_results"
-nvt_path = "./nvt_results"
+nvt_path = "/home/xiaoqi/molsim/co2_nvt_deduplicated"
 rst_path = "./parse_results_co2"
 
 def get_supercell(structure):
-    with open(os.path.join(nvt_path, "%s/simulation.input" %structure)) as f_input:
+    with open(os.path.join(nvt_path, "%s/simulation.input" %structure), "r") as f_input:
         for line in f_input:
             if re.search("UnitCells", line):
                 s1, s2, s3 = line.split()[1], line.split()[2], line.split()[3]
@@ -43,85 +45,112 @@ def get_supercell(structure):
     return supercell
 
 def get_lattice(structure):
-    with open(os.path.join(nvt_path, "%s/Movies/System_0/Framework_0_final.vasp" %structure)) as file:
+    with open(os.path.join(nvt_path, "%s/Movies/System_0/Framework_final.pdb" %structure)) as file:
         lines = file.readlines()
-        lines = lines[2:5]
-        lattice = [[float(l) for l in line.split()] for line in lines]
-    return lattice
+        lattice = np.array([float(l) for l in lines[1].split()[1:4]])
+        frame_ele = np.array([l.split()[2] for l in lines[2:]])
+        frame_pos = np.array([[float(t) for t in l.split()[4:7]] for l in lines[2:]])
+    return lattice, frame_ele, frame_pos
 
-def lj(sigma_lb, epsilon_lb, distance):
-    u_lj = 4 * epsilon_lb * ((sigma_lb/distance)**12 - (sigma_lb/distance)**6)
-    return u_lj
+def plot_distance_distribution(structure, dist):
+    fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 6))
+    axs.hist(dist, bins=100)
+    axs.set_xlabel("Distance (A)", fontdict={"fontsize": 20})
+    axs.set_ylabel("Times", fontdict={"fontsize": 20})
+    fig.savefig(os.path.join(rst_path, "dist_plots/%s.png" %structure))
+    plt.close(fig)
+    return None
 
+def plot_lj_distribution(structure, poten):
+    fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 6))
+    axs.hist(poten, bins=100)
+    axs.set_xlabel("Normalized Lennard-Jones potential", fontdict={"fontsize": 20})
+    axs.set_ylabel("Times", fontdict={"fontsize": 20})
+    fig.savefig(os.path.join(rst_path, "poten_plots/%s.png" %structure))
+    plt.close(fig)
+    return None
+
+    
 # Run the functions
 def run(structure):
-    print("Pharsing %s" %structure)
     sc = get_supercell(structure)
-    un_cry = Structure.from_file(os.path.join(nvt_path, "%s/%s.cif" %(structure, structure)))
+    latt, ele, pos = get_lattice(structure)
 
-    latt = get_lattice(structure)
-    un_cry.lattice = [np.array(latt[0])/sc[0], np.array(latt[1])/sc[1], np.array(latt[2])/sc[2]]
-
-
-    ele_sigma = [uff.loc[uff["element"] == site.specie.symbol]["sigma"].values for site in un_cry.sites]
-    ele_epsilon = [uff.loc[uff["element"] == site.specie.symbol]["epsilon"].values for site in un_cry.sites]
-    site_sigma_c = [(s+c_sigma)/2 for s in ele_sigma]
-    site_epsilon_c = [math.sqrt(e*c_epsilon) for e in ele_epsilon]
-    site_sigma_o = [(s+o_sigma)/2 for s in ele_sigma]
-    site_epsilon_o = [math.sqrt(e*o_epsilon) for e in ele_epsilon]
-    for i in range(len(un_cry.sites)):
-        un_cry.sites[i].properties = {
-            "lb_sigma_c": site_sigma_c[i], "lb_epsilon_c": site_epsilon_c[i], 
-            "lb_sigma_o": site_sigma_o[i], "lb_epsilon_o": site_epsilon_o[i],
-            "weight": []
-        }
-    
+    # read gas position
     with open(os.path.join(nvt_path, "%s/Movies/System_0/Movie_%s_%d.%d.%d_298.000000_0.000000_allcomponents.pdb" %(structure, structure, sc[0], sc[1], sc[2]))) as file:
         data = file.readlines()
-        lines = [line.split() for line in data if "ATOM" in line]
-        atom_pos_c = [np.array([float(t) for t in l[4:7]]) for l in lines if l[1] == '2']
-        atom_pos_o1 = [np.array([float(t) for t in l[4:7]]) for l in lines if l[1] == '1']
-        atom_pos_o2 = [np.array([float(t) for t in l[4:7]]) for l in lines if l[1] == '3']
+        positions = np.array([line.split() for line in data if "ATOM" in line])
+        pos_c = np.array([[float(t) for t in l[4:7]] for l in positions if l[1] == '2'])
+        pos_o1 = np.array([[float(t) for t in l[4:7]] for l in positions if l[1] == '1'])
+        pos_o2 = np.array([[float(t) for t in l[4:7]] for l in positions if l[1] == '3'])
     
-    sphere_sites = np.array([[site for site in un_cry.get_sites_in_sphere(pt=pos, r=threshold) if site.specie.symbol != "H"] 
-                                for pos in atom_pos_c], dtype=object)
-    print("atoms in the cutoff range: %s" %sphere_sites)
-
-    dist_c = np.array([np.array([site.distance_from_point(atom_pos_c[idx]) for site in sphere_sites[idx]]) 
-                        for idx in range(len(atom_pos_c))], dtype=object)
-    dist_o1 = np.array([np.array([site.distance_from_point(atom_pos_o1[idx]) for site in sphere_sites[idx]]) 
-                        for idx in range(len(atom_pos_o1))], dtype=object)
-    dist_o2 = np.array([np.array([site.distance_from_point(atom_pos_o2[idx]) for site in sphere_sites[idx]]) 
-                        for idx in range(len(atom_pos_o2))], dtype=object)
-    potential_c = np.array([np.array([lj(sphere_sites[i][j].properties["lb_sigma_c"], sphere_sites[i][j].properties["lb_epsilon_c"], dist_c[i][j]) 
-                            for j in range(len(sphere_sites[i]))]).flatten() 
-                            for i in range(len(atom_pos_c))], dtype=object)
-    potential_o1 = np.array([np.array([lj(sphere_sites[i][j].properties["lb_sigma_o"], sphere_sites[i][j].properties["lb_epsilon_o"], dist_o1[i][j]) 
-                            for j in range(len(sphere_sites[i]))]).flatten() 
-                            for i in range(len(atom_pos_o1))], dtype=object)
-    potential_o2 = np.array([np.array([lj(sphere_sites[i][j].properties["lb_sigma_o"], sphere_sites[i][j].properties["lb_epsilon_o"], dist_o2[i][j]) 
-                            for j in range(len(sphere_sites[i]))]).flatten() 
-                            for i in range(len(atom_pos_o2))], dtype=object)
-    potential = np.sum([potential_c, potential_o1, potential_o2], axis=0)
-    print(potential)
-
-    np.seterr(invalid='ignore')
-    mins = [po.min() if len(po)!=0 else np.nan for po in potential]
-    maxs = [po.max() if len(po)!=0 else np.nan for po in potential]
-    po_norm = np.array([-((po-min)/(max-min)-1) for min, max, po in zip(mins, maxs, potential)], dtype=object)
-    po_norm = np.hstack(po_norm)
-
-    idx = 0
-    for i in range(sphere_sites.shape[0]):
-        for j in range(len(sphere_sites[i])):
-            sphere_sites[i][j].properties["weight"].append(po_norm[idx])
-            idx += 1
-    weights = np.array([np.average(site.properties["weight"]) if len(site.properties["weight"])!=0 else 0 
-                            for site in un_cry.sites])
-    np.nan_to_num(weights, copy=False, nan=0)
     
-    print("%s Done" %structure)
-    return {structure: list(weights)}
+    # calculate distance with periodic boundary conditions
+    dist_c = np.array([np.abs([atom-c for atom in pos]) for c in pos_c])
+    dist_c = np.array([[[l-r if r>l/2 else r for r,l in zip(a, latt)] for a in s] for s in dist_c])
+    dist_c *= dist_c
+    dist_c = np.sum(dist_c, axis=2)
+    dist_c = np.sqrt(dist_c)
+
+    dist_o1 = np.array([np.abs([atom-o for atom in pos]) for o in pos_o1])
+    dist_o1 = np.array([[[l-r if r>l/2 else r for r,l in zip(a, latt)] for a in s] for s in dist_o1])
+    dist_o1 *= dist_o1
+    dist_o1 = np.sum(dist_o1, axis=2)
+    dist_o1 = np.sqrt(dist_o1)
+    
+    dist_o2 = np.array([np.abs([atom-o for atom in pos]) for o in pos_o2])
+    dist_o2 = np.array([[[l-r if r>l/2 else r for r,l in zip(a, latt)] for a in s] for s in dist_o2])
+    dist_o2 *= dist_o2
+    dist_o2 = np.sum(dist_o2, axis=2)
+    dist_o2 = np.sqrt(dist_o2)
+
+    # read sigma and epsilon
+    ele_sigma_c = np.array([(uff[uff["element"]==e]["sigma"].item()+c_sigma)/2 for e in ele])
+    ele_epsilon_c = np.array([np.sqrt(uff[uff["element"]==e]["epsilon"].item()*c_epsilon) for e in ele])
+
+    ele_sigma_o = np.array([(uff[uff["element"]==e]["sigma"].item()+o_sigma)/2 for e in ele])
+    ele_epsilon_o = np.array([np.sqrt(uff[uff["element"]==e]["epsilon"].item()*o_epsilon) for e in ele])
+
+    # calculate lj potentials
+    poten_c = np.array([ele_epsilon_c*((ele_sigma_c/r)**12-(ele_sigma_c/r)**6) for r in dist_c])
+    poten_o1 = np.array([ele_epsilon_o*((ele_sigma_o/r)**6-(ele_sigma_o/r)**12) for r in dist_o1])
+    poten_o2 = np.array([ele_epsilon_o*((ele_sigma_o/r)**6-(ele_sigma_o/r)**12) for r in dist_o2])
+
+    avg_poten_c = np.average(poten_c, axis=0)
+    avg_poten_o1 = np.average(poten_o1, axis=0)
+    avg_poten_o2 = np.average(poten_o2, axis=0)
+
+    avg_poten = np.array([c+o1+o2 for c, o1, o2 in zip(avg_poten_c, avg_poten_o1, avg_poten_o2)])
+
+    # map the calculated potentials to one unit cell
+    num_uc = sc[0] * sc[1] * sc[2]
+    num_at = int(len(pos) / num_uc)
+    for i in range(num_uc):
+        if i == 0:
+            dist_uc = dist_c[0:num_at]
+            poten_uc = avg_poten[0:num_at]
+        else:
+            dist_uc += dist_c[i*num_at:(i+1)*num_at]
+            poten_uc += avg_poten[i*num_at:(i+1)*num_at]
+    dist_uc /= num_uc
+    poten_uc /= num_uc
+
+    # normalization
+    scaler_poten = MinMaxScaler()
+    poten_norm = scaler_poten.fit_transform(poten_uc.reshape(-1, 1))
+
+    scaler_dist = MinMaxScaler()
+    dist_norm = scaler_dist.fit_transform(dist_uc.reshape(-1, 1))
+
+    plot_distance_distribution(structure, dist_norm)
+    plot_lj_distribution(structure, poten_norm)
+
+    return {
+        structure: {
+            "dist": list(dist_norm.reshape(-1,)),
+            "poten": list(poten_norm.reshape(-1,))
+        }
+    }
 
 
 if __name__ == '__main__':
@@ -131,9 +160,10 @@ if __name__ == '__main__':
         if os.path.exists(os.path.join(nvt_path, "%s/Movies/System_0/Framework_0_final.vasp" %structure)) == False:
             print("%s undone!" %structure)
             structure_lst.remove(structure)
-    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+       
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         result = list(executor.map(run, structure_lst))
     for r in result:
         dumped.update(r)
-    with open(os.path.join(rst_path, "weights_%s.json" %threshold), "w") as file:
+    with open(os.path.join(rst_path, "weights.json"), "w") as file:
         json.dump(dumped, file, indent=4)
